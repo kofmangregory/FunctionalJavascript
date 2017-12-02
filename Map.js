f0e92aededcf4ca5850fdeb7b8b72694c9166fa8
Gregory <kofmangregory@gmail.com>
2017-12-02 01:24:14 -0500

    Added Map.js

---
+++ Map.js
@@ -0,0 +1,24 @@
+/* 
+* Task:
+* Convert the following code from a for-loop to Array#map:
+*
+*    function doubleAll(numbers) {
+*      var result = []
+*      for (var i = 0; i < numbers.length; i++) {
+*        result.push(numbers[i] * 2)
+*      }
+*      return result
+*    }
+*
+*    module.exports = doubleAll
+*/
+
+function doubleAll(numbers) {
+
+	var result = numbers.map(function(num) {
+		return num * 2;
+	});
+	return result;
+}
+
+module.exports = doubleAll;
